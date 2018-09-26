c47b51ad7dccf6cae2a4c010abad8517e13747df
Adam Laszlo Horvath <ahorvath@hortonworks.com>
2018-09-26 11:45:38 +0200

    BUG-111253 [E2E] recovery tests error handling

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/UnhealthyNodeStrategy.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/newway/UnhealthyNodeStrategy.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sequenceiq.cloudbreak.api.model.FailureReport;
 import com.sequenceiq.cloudbreak.api.model.stack.StackResponse;
 import com.sequenceiq.cloudbreak.api.model.stack.instance.InstanceGroupResponse;
@@ -11,6 +15,9 @@ import com.sequenceiq.cloudbreak.client.ConfigKey;
 import com.sequenceiq.it.IntegrationTestContext;
 
 public class UnhealthyNodeStrategy implements Strategy {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnhealthyNodeStrategy.class);
+
     private final String hostgroup;
 
     private final int nodeCount;
@@ -30,19 +37,28 @@ public class UnhealthyNodeStrategy implements Strategy {
                 .filter(ig -> ig.getGroup().equals(hostgroup)).collect(Collectors.toList()).get(0);
         List<String> nodes = instanceGroup.getMetadata().stream()
                 .map(metadata -> metadata.getDiscoveryFQDN()).collect(Collectors.toList()).subList(0, nodeCount);
-        ProxyCloudbreakClient client = getAutoscaleProxyCloudbreakClient(integrationTestContext);
+        CloudbreakClient client = getAutoscaleCloudbreakClient(integrationTestContext);
         FailureReport failureReport = new FailureReport();
         failureReport.setFailedNodes(nodes);
-        client.autoscaleEndpoint().failureReport(id, failureReport);
+        client.getCloudbreakClient().autoscaleEndpoint().failureReport(id, failureReport);
     }
 
-    private ProxyCloudbreakClient getAutoscaleProxyCloudbreakClient(IntegrationTestContext integrationTestContext) {
-        return new ProxyCloudbreakClient(
+    private CloudbreakClient getAutoscaleCloudbreakClient(IntegrationTestContext integrationTestContext) {
+        String autoscaleClientSecret = integrationTestContext.getContextParam(CloudbreakTest.AUTOSCALE_SECRET);
+        String autoscaleClientId = integrationTestContext.getContextParam(CloudbreakTest.AUTOSCALE_CLIENTID);
+        if (StringUtils.isNotEmpty(autoscaleClientId) && StringUtils.isNotEmpty(autoscaleClientSecret)) {
+            CloudbreakClient client = new CloudbreakClient();
+            client.setCloudbreakClient(new ProxyCloudbreakClient(
                     integrationTestContext.getContextParam(CloudbreakTest.CLOUDBREAK_SERVER_ROOT),
                     integrationTestContext.getContextParam(CloudbreakTest.IDENTITY_URL),
-                    integrationTestContext.getContextParam(CloudbreakTest.AUTOSCALE_SECRET),
-                    integrationTestContext.getContextParam(CloudbreakTest.AUTOSCALE_CLIENTID),
-                    new ConfigKey(false, true, true));
+                    autoscaleClientSecret,
+                    autoscaleClientId,
+                    new ConfigKey(false, true, true)));
+            return client;
+        } else {
+            LOGGER.error("Cannot create Autoscale client, fall back to default client.");
+            return CloudbreakClient.getTestContextCloudbreakClient().apply(integrationTestContext);
+        }
 
     }
 }
